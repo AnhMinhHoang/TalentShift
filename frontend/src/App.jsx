c1546f7a3db4008c9262ad3e7afaac88073ba30f
Hoàng Anh Minh <144108076+AnhMinhHoang@users.noreply.github.com>
2025-02-21 12:51:03 +0700

    Fix js script
    Co-authored-by: Đinh Văn Thái Sơn <139418331+dvtson2004@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Index from './pages/index'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Index />
+      <Footer />
     </div>
   )
 }
